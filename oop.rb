2ae4ade76aba9d74359fd78f644c3e1db4a6c74a
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-06 21:47:52 -0600

    Add Vampire and Unicorn classes to oop.rb

--- oop.rb
+++ oop.rb
@@ -3,13 +3,51 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn
+    def initialize(name, color="silver")
+        @name = name
+        @color = color
+    end
 
+    def say(str)
+        "*~* #{str} *~*"
+    end
+
+    def get_name
+        @name
+    end
+
+    def get_color
+        @color
+    end
+end
+
+# uni1 = Unicorn.new("My Little Pony", "blue")
+# puts uni1.say("#{uni1.get_name} has the most beautiful #{uni1.get_color} coat.")
 
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
+
+class Vampire
+    attr_reader :name, :pet, :thirsty
+    def initialize(name, pet="bat", thirsty=true)
+        @name = name
+        @pet = pet
+        @thirsty = thirsty
+    end
+
+    def drink
+        @thirsty = false
+    end
+end
+
+# dracula = Vampire.new("Dracula")
+# puts "name: #{dracula.name}, pet: #{dracula.pet}, thirsty?: #{dracula.thirsty}"
+# dracula.drink
+# puts "thirsty?: #{dracula.thirsty}"
 
 
 #  Write a Dragon class
@@ -18,7 +56,6 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
-
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
