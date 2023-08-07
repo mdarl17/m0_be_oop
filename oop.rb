eb575edef2b9c9587ea19f3cf0b8cc589a3e0720
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-06 22:53:12 -0600

    Add Dragon and Hobbit classes to oop.rb

--- oop.rb
+++ oop.rb
@@ -49,13 +49,32 @@ end
 # dracula.drink
 # puts "thirsty?: #{dracula.thirsty}"
 
-
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
 #  it should have a dynamic rider attribute (string)
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+
+class Dragon
+    attr_accessor :name, :rider, :color, :is_hungry, :eaten_today
+    def initialize(name, rider, color, is_hungry=true)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = is_hungry
+        @eaten_today = 0
+    end
+
+    def eat
+        @eaten_today = @eaten_today + 1
+        if @eaten_today >= 4
+            @is_hungry = false
+        end
+    end
+end
+
+# drago = Dragon.new("Drago", "Leonitus", "green", true)
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -65,3 +84,19 @@ end
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+    attr_accessor :name, :disposition, :age, :is_adult, :is_old, :has_ring
+    def initialize(name, disposition, age=0, is_adult=false, is_old=false, has_ring=false)
+        @name = name
+        @disposition = disposition
+        @age = age
+        @is_adult = @age >= 33 ? true : false
+        @is_old = @age >= 101 ? true : false
+        @has_ring = @name == "Frodo" ? true : false
+    end
+
+    def celebrate_birthday
+        @age = @age + 1
+    end
+end
